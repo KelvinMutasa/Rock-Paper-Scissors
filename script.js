1cb31c964ccfe8e46eb66e7e68f0026d6c71ac84
Kelvin <48760423+KelvinMutasa@users.noreply.github.com>
2024-08-09 00:23:42 +0200

    almost

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 /* Variable declaration*/
 
 const round = document.getElementById("roundNumber");
-const playerScore =document.getElementById("");
+const playerScore = document.getElementById("playerScore");
+const compScore = document.getElementById("compScore");
+const playerChoice = document.getElementById("playerChoice");
+const compChoice = document.getElementById("CompChoice");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+let compoptions = ['rock','paper','scissors'];
+
